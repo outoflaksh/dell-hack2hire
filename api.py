397aa8738bd61f7fad5bdf15afd9054f9990d9cf
Lakshya <lakshyam38@gmail.com>
2022-11-21 17:43:46 +0530

    Wrapped script in REST API

--- api.py
+++ api.py
@@ -1,8 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
+from main import generate_doc_for_codebase
 
 app = FastAPI()
 
 
 @app.get("/")
 def read_index():
-    return {"msg": "hey it works!"}
+    return "<h1> Home page </h1>"
+
+
+@app.post("/generate-document")
+def generate_document(codebase_path: str = Form(), config_file_name: str = Form()):
+
+    return generate_doc_for_codebase(
+        codebase_path=codebase_path, config_file_name=config_file_name
+    )
